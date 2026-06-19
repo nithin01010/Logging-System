8e76217bfa3932df139e9665ae9c00df17fcba59
nithin01010 <153373802+nithin01010@users.noreply.github.com>
2026-06-19 21:02:19 +0530

    Added Dockerfile, docker-compose and config.py

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Observability Platform"
+    MONGO_URI: str
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
