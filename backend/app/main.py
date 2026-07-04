93b4a2973c12f6a884bce5d7869468a405634219
nithin01010 <153373802+nithin01010@users.noreply.github.com>
2026-07-04 21:58:00 +0530

    Fixed main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,30 @@
-from fastapi import Fastapi
-from fast.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
-asyn def lifespan(app: Fastapi):
+async def lifespan(app: FastAPI):
     print("Starting up service")
     yield
     print("Shutting down service")
 
-app = Fastapi(
-        title="Logging System",
-        description="The logging system is built with Graphql along with Next js"
-        )
+
+app = FastAPI(
+    title="Logging System",
+    description="The logging system is built with Graphql along with Next js",
+    lifespan=lifespan
+)
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"]
-        )
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/health', tags=["Health"])
+
+@app.get("/health", tags=["Health"])
 async def health_check():
-    return {
-            "status": "healthy",
-            "database": "disconnected"
-            }
+    return {"status": "healthy", "database": "disconnected"}
